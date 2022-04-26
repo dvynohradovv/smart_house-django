99cf338f943b5375bdb8f7b6d6dd7f05867a847c
dvynohradovv <55922843+dvynohradovv@users.noreply.github.com>
2022-04-26 19:50:30 +0300

    Auto pep8

--- smart_house/celery.py
+++ smart_house/celery.py
@@ -7,12 +7,12 @@ from celery import Celery
 os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'smart_house.settings')
 django.setup()
 
-
 app = Celery('proj')
 app.config_from_object('django.conf:settings', namespace='CELERY')
 app.autodiscover_tasks()
 
 from smart_house.core.tasks import smart_home_manager
+
 
 @app.on_after_configure.connect
 def setup_periodic_tasks(sender, **kwargs):
--- smart_house/core/tasks.py
+++ smart_house/core/tasks.py
@@ -3,6 +3,7 @@ from celery import task
 
 from .models import Setting
 
+
 @task()
 def smart_home_manager():
     # Здесь ваш код для проверки условий
