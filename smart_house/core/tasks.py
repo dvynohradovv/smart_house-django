2236db0b19c0f27dfb73ca0927433acbbeaff7cd
dvynohradovv <55922843+dvynohradovv@users.noreply.github.com>
2022-05-03 18:34:25 +0300

    Add request_controller_newdata

--- smart_house/core/tasks.py
+++ smart_house/core/tasks.py
@@ -1,7 +1,29 @@
 from __future__ import absolute_import, unicode_literals
+import tempfile
 import celery
+import os
+import json
+
+import requests.exceptions
+from django.conf import settings
+from requests import get
 
 from .models import Setting
+
+AUTH_HEADERS = {'Authorization': f"Bearer {settings.SMART_HOME_ACCESS_TOKEN}"}
+
+
+def request_controller_newdata():
+    try:
+        raw_data = get(settings.SMART_HOME_API_URL, headers=AUTH_HEADERS).json()["data"]
+    except requests.exceptions.RequestException:
+        raw_data = []
+
+    data = {controller["name"]: controller["value"] for controller in raw_data}
+    with open(os.path.join(tempfile.gettempdir(), "controller.json"), 'w') as fs:
+        json.dump(data, fs)
+
+    return data
 
 
 @celery.task()
